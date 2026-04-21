c0715a8a75da0a272b0df2b56274ca5a7c0aaf23
Gopal Kumar Pandit <gopalkpandit07@gmail.com>
2026-04-21 17:03:35 +0530

    fix Readme File

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # calculator-app
-Basic Calculation For Using Python and Tkinter
+This Is A Basic Calculator Application Built Using Python And Tkinter
+It Performs Simple Arithmatic Operations Like 
+Addition, Subtraction, Multiplication, And Division
+
+## Features
+##### Simple And Clean UI
+##### Performs Basic Calculatin
+
+## How To Run
+### 1. Clone the Repository
+``` bash
+git clone
+```
+
+### 2. Go To Project Folder
+```bash
+cd your-project-folder
+```
+
+### 3. Run The Application
+```bash
+python main.py
+```
+A GUI window will open (calculator app)
+
+## Author
+-  [@Gopal](https://github.com/gopalkpandit)
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ def calculate():
     a = int(entry1.get())
     b = int(entry2.get())
 
-    result.set("Add: " + str(calculator.add(a, b)))
-    result.set(result.get() + "\nSub: " + str(calculator.sub(a, b)))
-    result.set(result.get() + "\nMult: " + str(calculator.mult(a, b)))
+    result.set("Addition: " + str(calculator.add(a, b)))
+    result.set(result.get() + "\nSubstraction: " + str(calculator.sub(a, b)))
+    result.set(result.get() + "\nMultiplication: " + str(calculator.mult(a, b)))
     result.set(result.get() + "\nDivision: " + str(calculator.divide(a, b)))
 
 root = tk.Tk()
@@ -15,15 +15,15 @@ root.title("Calculator App")
 root.geometry("300x250")
 root.configure(bg="lightblue")
 
-entry1 = tk.Entry(root, font=("Arial",14))
+entry1 = tk.Entry(root, font=("Arial",10))
 entry1.pack(pady=10)
 
-entry2 = tk.Entry(root, font=("Arial",14) )
+entry2 = tk.Entry(root, font=("Arial",10) )
 entry2.pack(pady=10)
 
 result = tk.StringVar()
 
-btn = tk.Button ( root, text="Add", bg="green", fg="white", command=calculate)
+btn = tk.Button ( root, text="Calculate", bg="green", fg="white", command=calculate)
 btn.pack(pady=10)
 
 label = tk.Label(root, textvariable=result, bg="lightblue", font=("Arial",14))
